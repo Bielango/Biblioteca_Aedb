161004ce687541fac74f1405a0ea8369d5141a04
Ragaz79 <gabriel.oragazzi@gmail.com>
2024-08-25 22:10:27 -0300

    comeco

---
+++ Sistema_Biblioteca.Windows/FrmMenu.cs
@@ -0,0 +1,15 @@
+namespace Sistema_Biblioteca.Windows
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
