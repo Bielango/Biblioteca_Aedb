2d51acd73dfe2c102cb3368a238143c46e49e1c2
Gabriel_Gomes <=>
2024-09-22 14:43:09 -0300

    .

--- Sistema_Biblioteca.Windows/obj/Debug/net8.0-windows/Sistema_Biblioteca.Windows.AssemblyInfo.cs
+++ Sistema_Biblioteca.Windows/obj/Debug/net8.0-windows/Sistema_Biblioteca.Windows.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_Biblioteca.Windows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b790cbd99ab7fdb586409144163ba947ff2541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542390a2a512e7f1142654b3569acde5b3dafb2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_Biblioteca.Windows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema_Biblioteca.Windows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
